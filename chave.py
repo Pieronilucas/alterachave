6f37d5d90b5aa857dc5d70461c166cf77719ceb8
Lucas Pieroni <lucaspieroni819918@outlook.com>
2024-06-05 10:14:14 -0300

    Add files via upload

---
+++ chave.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title(':blue[ARRUMAR CHAVES/CNPJs/CPFs]')
+chave_arrumada = st.text_input('chave: ').replace('-', '').replace('/','').replace('.','')
+st.info(chave_arrumada)
