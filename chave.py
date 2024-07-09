08059b8d60193ca8fc92488605c47fb625291942
Lucas Pieroni <lucaspieroni819918@outlook.com>
2024-07-09 20:14:31 -0300

    Update chave.py

--- chave.py
+++ chave.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title(':blue[ARRUMAR CHAVES/CNPJs/CPFs]')
-chave_arrumada = st.text_input('chave: ').replace('-', '').replace('/','').replace('.','')
+chave_arrumada = st.text_input('chave: ').replace('-', '').replace('/','').replace('.','').replace(' ','')
 st.info(chave_arrumada)
